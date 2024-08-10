38d0a75f4a2b9b5da125d02fbaef972cf3f4b777
Subham Bharadwaz <subhamsbharadwaz@gmail.com>
2024-08-10 16:06:11 +0530

    feat: :sparkles: Added CORS

--- cmd/calculator-api/main.go
+++ cmd/calculator-api/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"net/http"
 
+	"github.com/rs/cors"
 	"github.com/subhamBharadwaz/go-calculator-api/internal/logger"
 	"github.com/subhamBharadwaz/go-calculator-api/internal/middleware"
 	"github.com/subhamBharadwaz/go-calculator-api/internal/routes"
@@ -12,11 +13,16 @@ func main() {
 	logger := logger.NewLogger()
 	router := routes.NewRouter(logger)
 
+	corsHandler := cors.Default().Handler(router)
+
+	handler := middleware.Logging(logger, corsHandler)
+
 	server := http.Server{
 		Addr:    ":8080",
-		Handler: middleware.Logging(logger, router),
+		Handler: handler,
 	}
 
 	logger.Info("Server listening on port :8080")
+
 	server.ListenAndServe()
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/subhamBharadwaz/go-calculator-api
 
 go 1.22.4
+
+require github.com/rs/cors v1.11.0 // indirect
